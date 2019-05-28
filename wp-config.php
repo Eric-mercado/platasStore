bf9a7e0f3a355733ea28a8ed7a449142e0bccef0
eric mercado <ericmercado@macMatrix.local>
2019-05-27 21:37:23 -0700

    first commit

---
+++ wp-config.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', '/Applications/MAMP/htdocs/wordpress/wp-content/plugins/wp-super-cache/' );
+define( 'DB_NAME', 'wp_db' );
+
+/** MySQL database username */
+define( 'DB_USER', 'drupal' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'drupal' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '8{;ZMY5BZWazcH-qgLO[ZTue%>_7}(^gSe}=fogq;%c=;CrG;N.q8mf~5y!Wap:4' );
+define( 'SECURE_AUTH_KEY',  'uAlp+}dW(l}`Q(rxz$[5~a-v--f/G5@~ !Oal+r ,T1V3$B^=gGx.tHKg3gt?y.J' );
+define( 'LOGGED_IN_KEY',    '=:BT`kFCg0IIK!S0l_(=?H.G$#B.l-.TRx$?U_~QhZO<.~BcFxBkR3sMz|Z%3|@j' );
+define( 'NONCE_KEY',        'wqvQh-_)+tH#QD^?BEq/AS_$~L,FdKDP[:SV6PM&)uP(])@TB)d*#]tT<pLaEVTP' );
+define( 'AUTH_SALT',        '+8|.4(Xxyx=(Ye7tP2 PG-/)=/5iOy?7HfhCiL[mjHv&!d7c4Md`e^V->R1dDUxk' );
+define( 'SECURE_AUTH_SALT', 'krc.!QFn(~Kbg[J^ <Pqd8vanC#3!q]DXV=,os(pIQJi={j=^y^tc(Pm|P.@EX/D' );
+define( 'LOGGED_IN_SALT',   ',E+4 _Rl/4KVfteb>P[}-qB^lF0.|h1e(Jj$yXuHE-_~k9cJhcxlHKSMjwrLze3V' );
+define( 'NONCE_SALT',       'zYUAan9$u/)Wji2SbOW<Ex~~vkZ!f1|!r`JU|.G[K15nP|zIt7#LGj2sWU27Ar}c' );
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
---
+++ wp-content/themes/vantage/inc/contributors.php
@@ -0,0 +1,73 @@
+<?php
+return json_decode( '{
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 1474,
+		"score": 78.52581464080393,
+		"percent": 54.767299250464276
+	},
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 1673,
+		"score": 26.48181255409879,
+		"percent": 18.469561372641902
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 2184,
+		"score": 13.153403556832664,
+		"percent": 9.173752504884561
+	},
+	"5cff8d4385a469d1baab761889ad3161": {
+		"name": "Alex S",
+		"email": "5cff8d4385a469d1baab761889ad3161",
+		"loc": 163,
+		"score": 12.844516385622606,
+		"percent": 8.958321232790501
+	},
+	"43c0e7220c23bd6636cf5469be6e9612": {
+		"name": "Braam Genis",
+		"email": "43c0e7220c23bd6636cf5469be6e9612",
+		"loc": 166,
+		"score": 8.05592121973424,
+		"percent": 5.618547864769192
+	},
+	"9e7a3b2d24c2b15c53209ba8e7b4e724": {
+		"name": "Kevin Batdorf",
+		"email": "9e7a3b2d24c2b15c53209ba8e7b4e724",
+		"loc": 22,
+		"score": 1.8662105989398556,
+		"percent": 1.3015759824211723
+	},
+	"d1bea7e3f82e561c135bf5a3d4f9f896": {
+		"name": "SiteOrigin Support",
+		"email": "d1bea7e3f82e561c135bf5a3d4f9f896",
+		"loc": 13,
+		"score": 1.2152238656689478,
+		"percent": 0.8475496804692034
+	},
+	"d203744279876c5eda043108e2611648": {
+		"name": "Alex S",
+		"email": "d203744279876c5eda043108e2611648",
+		"loc": 19,
+		"score": 0.7579483014057169,
+		"percent": 0.5286259254914877
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 22,
+		"score": 0.39091665979832696,
+		"percent": 0.27264218508396143
+	},
+	"07294cc1dd7658895e44c559dfffd76f": {
+		"name": "gpriday",
+		"email": "07294cc1dd7658895e44c559dfffd76f",
+		"loc": 21,
+		"score": 0.08907391550720362,
+		"percent": 0.06212400098373112
+	}
+}', true );
